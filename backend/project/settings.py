ffb2d87b18c53c22b5048b1b4587e822b8fca2ab
Leonardo Melo <leonardo.mel04@outlook.com>
2023-04-25 12:33:53 -0300

    Create APP Ecommerce

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -34,8 +34,10 @@ INSTALLED_APPS = [
     "django_extensions",
     "django_filters",
     "rest_framework",
+    "rest_framework.authtoken",
     # START APP#
     "core",
+    "ecommerce",
 ]
 
 
@@ -126,6 +128,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "EXCEPTION_HANDLER": "rest_framework_json_api.exceptions.exception_handler",
     "DEFAULT_PARSER_CLASSES": ("rest_framework_json_api.parsers.JSONParser",),
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework_json_api.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
